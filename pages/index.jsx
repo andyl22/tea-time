790d2bfe798788c855f23a12bf6cb01cf2c44cce
Andy <landywl4@gmail.com>
2022-07-11 21:37:34 -0700

    Added basic timer functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,27 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   const [time, setTime] = useState(0);
+  const [interval, setInterval] = useState(5);
+  const [timeHistory, setTimeHistory] = useState([0]);
   const selectableIntervals = [5, 10, 15, 30, 60];
+
   const startTimer = () => {
-    console.log('started');
+    const newTime = timeHistory[timeHistory.length - 1] + interval;
+    setTimeHistory([...timeHistory, newTime]);
+    setTime(newTime);
   };
+
+  useEffect(() => {
+    let timer;
+    if (time > 0) {
+      timer = setTimeout(() => setTime(time - 1), 1000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [time]);
 
   return (
     <div className={styles.container}>
@@ -24,9 +38,11 @@ export default function Home() {
       <main className={styles.main}>
         <span className={styles.timer}>{time}</span>
         <button onClick={startTimer}>Start</button>
-        <select onChange={(e) => setInterval(e.target.value)}>
-          {selectableIntervals.map((interval) => (
-            <option>{interval}</option>
+        <select
+          onChange={(e) => setInterval(parseInt(e.target.value, 10))}
+        >
+          {selectableIntervals.map((int) => (
+            <option>{int}</option>
           ))}
         </select>
       </main>
